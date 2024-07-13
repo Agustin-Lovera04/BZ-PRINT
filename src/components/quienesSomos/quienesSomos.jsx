91cd2c56482c109fd68f5920300def88a2bfbf5d
Agustin-Lovera04 <agusslovera04@gmail.com>
2024-07-13 14:46:32 -0300

    1º Commit Estructura-Main

---
+++ src/components/quienesSomos/quienesSomos.jsx
@@ -0,0 +1,16 @@
+import './quienesSomos.css'
+
+const QuienesSomos = () => {
+    return (
+        <>
+        <div className="quienesSomos-Container">
+            <h1><span className="TitleQuienesSomos-Content fw-bold">¿QUIENES </span><span className="TitleQuienesSomos2-Content fw-bold">SOMOS?</span></h1>
+
+            <iframe src="https://www.instagram.com/p/C8pyoc3xAWc/" width="280" height="505" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
+
+        </div>
+        </>
+    )
+}
+
+export default QuienesSomos
