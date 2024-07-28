2587dba8ff1d045a027c7a95f6dd94cb132bbf7a
Agustin <agusrugbyy@gmail.com>
2024-07-28 18:19:18 -0300

    Port Notebook

--- src/components/pregFrec/pregFrec.jsx
+++ src/components/pregFrec/pregFrec.jsx
@@ -7,21 +7,29 @@ const PreguntasFrecuentes = () => {
             <h1 className="titlePregFrec fw-bold col-12">PREGUNTAS FRECUENTES</h1>
 
             <div className="pregBoxs-Container col-12">
-                <div className="box">
+                <div className="box col-12">
+                    <div className="subBox">
                     <h3>PREGUNTA</h3>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex placeat provident labore tempore porro, illo recusandae neque natus maxime, et, minus itaque? Nesciunt deserunt, ipsam aut nobis temporibus fugiat ad!</p>
+                    </div>
                 </div>
-                <div className="box box-lft">
+                <div className="box col-12">
+                    <div className="subBox">
                     <h3>PREGUNTA</h3>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex placeat provident labore tempore porro, illo recusandae neque natus maxime, et, minus itaque? Nesciunt deserunt, ipsam aut nobis temporibus fugiat ad!</p>
+                    </div>
                 </div>
-                <div className="box">
+                <div className="box col-12">
+                <div className="subBox">
                     <h3>PREGUNTA</h3>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex placeat provident labore tempore porro, illo recusandae neque natus maxime, et, minus itaque? Nesciunt deserunt, ipsam aut nobis temporibus fugiat ad!</p>
+                    </div>
                 </div>
-                <div className="box box-lft">
+                <div className="box col-12">
+                <div className="subBox">
                     <h3>PREGUNTA</h3>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex placeat provident labore tempore porro, illo recusandae neque natus maxime, et, minus itaque? Nesciunt deserunt, ipsam aut nobis temporibus fugiat ad!</p>
+                    </div>
                 </div>
             </div>
         </div>
