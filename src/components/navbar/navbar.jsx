38513c8abbf0a377048e426a34478c59a09d626e
Agustin-Lovera04 <agusslovera04@gmail.com>
2024-08-15 23:49:37 -0300

    1º Commit Estructura-Main

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,51 +3,51 @@ import LogoBZ from '/images/BZPrint-Logo.png'
 import './navbar.css'
 
 
-const Navbar =  ({ scrollToMain, scrollToQuienesSomos, scrollToServicios, scrollToEnvios, scrollToPreguntasFrecuentes, scrollToEquipo, scrollToContacto }) => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
-        <div className="container-fluid navbar">
+const Navbar = ({ scrollToMain, scrollToQuienesSomos, scrollToServicios, scrollToEnvios, scrollToPreguntasFrecuentes, scrollToEquipo, scrollToContacto }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+      <div className="container-fluid navbar">
         <Link to='/#' className="navbar-brand">
           <button className="logoBtn-Navbar" onClick={scrollToMain}>
             <img src={LogoBZ} alt="Logo" className='logoImg-Navbar' />
           </button>
         </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-  
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <button className="nav-link" onClick={scrollToQuienesSomos}>Quienes Somos</button>
-              </li>
-              <div className="vertical-divider-NavBar col-1"></div>
-              <li className="nav-item">
-                <button className="nav-link" onClick={scrollToServicios}>Servicios</button>
-              </li>
-              <div className="vertical-divider-NavBar col-1"></div>
-              <li className="nav-item">
-                <button className="nav-link" onClick={scrollToEnvios}>Envios</button>
-              </li>
-              <div className="vertical-divider-NavBar col-1"></div>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
 
-              <li className="nav-item">
-                <button className="nav-link" onClick={scrollToPreguntasFrecuentes}>Preguntas Frecuentes</button>
-              </li>
-              <div className="vertical-divider-NavBar col-1"></div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <button className="nav-link" onClick={scrollToQuienesSomos}>Quienes Somos</button>
+            </li>
+            <div className="vertical-divider-NavBar col-1"></div>
+            <li className="nav-item">
+              <button className="nav-link" onClick={scrollToServicios}>Servicios</button>
+            </li>
+            <div className="vertical-divider-NavBar col-1"></div>
+            <li className="nav-item">
+              <button className="nav-link" onClick={scrollToEnvios}>Envios</button>
+            </li>
+            <div className="vertical-divider-NavBar col-1"></div>
 
-              <li className="nav-item">
-                <button className="nav-link" onClick={scrollToEquipo}>Nuestro Equipo</button>
-              </li>
-              <div className="vertical-divider-NavBar col-1"></div>
+            <li className="nav-item">
+              <button className="nav-link" onClick={scrollToPreguntasFrecuentes}>Preguntas Frecuentes</button>
+            </li>
+            <div className="vertical-divider-NavBar col-1"></div>
 
-              <li className="nav-item">
-                <button className="nav-link" onClick={scrollToContacto}>Contacto</button>
-              </li>
-            </ul>
-          </div>
+            <li className="nav-item">
+              <button className="nav-link" onClick={scrollToEquipo}>Nuestro Equipo</button>
+            </li>
+            <div className="vertical-divider-NavBar col-1"></div>
+
+            <li className="nav-item">
+              <button className="nav-link" onClick={scrollToContacto}>Contacto</button>
+            </li>
+          </ul>
         </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  )
 }
 export default Navbar
